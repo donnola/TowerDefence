f5effe7aa51891ccd97f3081d2d367ef444d5a27
donnola <gabbasova.aa@phystech.edu>
2021-02-15 23:11:22 +0300

    .

--- Assets/Scripts/GravitationMove.cs
+++ Assets/Scripts/GravitationMove.cs
@@ -17,31 +17,31 @@ public class GravitationMove : MonoBehaviour
     private float m_G;
     [SerializeField]
     private Vector3 m_R;
-
-    private float _m = 1.0f;
+    
     private Vector3 _a;
-    private Vector3 _initPos;
+    private float _initDistant;
     private bool _goToCenter = false;
 
     private void Awake()
     {
         transform.position = m_rStart;
-        _initPos = m_rStart;
+        _initDistant =(m_R - m_rStart).magnitude;
     }
 
     void FixedUpdate()
     {
-        if ((m_R - _initPos).magnitude < (m_R - m_rStart).magnitude & _goToCenter)
+        if (_initDistant < (m_R - m_rStart).magnitude & _goToCenter)
         {
             return;
         }
-        float square_distance = (m_R - m_rStart).magnitude * (m_R - m_rStart).magnitude;
-        _a = m_G * _m * m_M * (m_R - m_rStart).normalized / square_distance;
+
+        float distance = (m_R - m_rStart).magnitude;
+        _a = m_G * m_M * (m_R - m_rStart).normalized / (distance * distance);
         float time = Time.fixedDeltaTime;
         m_rStart += m_VStart * time + _a * time * time / 2;
         m_VStart += _a * time;
         transform.position = m_rStart;
-        if ((m_R - _initPos).magnitude > (m_R - m_rStart).magnitude)
+        if (_initDistant > (m_R - m_rStart).magnitude)
         {
             _goToCenter = true;
         }
