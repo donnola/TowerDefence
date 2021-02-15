61b81e0b021caaf83e99b2229ed73c1c701208e1
donnola <gabbasova.aa@phystech.edu>
2021-02-16 01:09:35 +0300

    .

--- Assets/Scripts/GravitationMove.cs
+++ Assets/Scripts/GravitationMove.cs
@@ -17,33 +17,25 @@ public class GravitationMove : MonoBehaviour
     private float m_G;
     [SerializeField]
     private Vector3 m_R;
-    
     private Vector3 _a;
-    private float _initDistant;
     private bool _goToCenter = false;
 
     private void Awake()
     {
         transform.position = m_rStart;
-        _initDistant =(m_R - m_rStart).magnitude;
     }
 
     void FixedUpdate()
     {
-        if (_initDistant < (m_R - m_rStart).magnitude & _goToCenter)
-        {
-            return;
-        }
-
         float distance = (m_R - m_rStart).magnitude;
         _a = m_G * m_M * (m_R - m_rStart).normalized / (distance * distance);
+        if (_a.magnitude > 5)
+        {
+            _a = _a.normalized * 5;
+        }
         float time = Time.fixedDeltaTime;
         m_rStart += m_VStart * time + _a * time * time / 2;
         m_VStart += _a * time;
         transform.position = m_rStart;
-        if (_initDistant > (m_R - m_rStart).magnitude)
-        {
-            _goToCenter = true;
-        }
     }
 }
